041777f053d56836f7422e4daa95bd6b0700152d
bacontree <arseny89@gmail.com>
2016-12-01 23:25:30 -0500

    Add files via upload
    System calls and find process kernel module

---
+++ printGroup.c
@@ -0,0 +1,32 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/syscalls.h>
+#include <linux/tty.h>
+
+#include "printGroup.h"
+
+asmlinkage long sys_print_group(int location) {
+
+	if (location != 0) {
+		printk("Stefan Chu: Wumbology. The Study of Wumbo.\n");
+		printk("Arseni Moguilevski: The inner machinations of my mind are an enigma.\n");
+		return 0;
+	}
+	else {
+		struct tty_struct *my_tty;
+		my_tty = current->signal->tty;
+		
+		if (my_tty != NULL) {
+			((my_tty->ops)->write) (my_tty, "Stefan: HELLO", strlen("Stefan: HELLO"));
+			((my_tty->ops)->write) (my_tty, "\015\012", 2);
+			((my_tty->ops)->write) (my_tty, "Arseni: WORLD", strlen("Arseni: WORLD"));
+			((my_tty->ops)->write) (my_tty, "\015\012", 2);
+			return 0;
+		}
+		else {
+			printk(KERN_ERR "Could not establish tty device\n");
+			return -ENODEV;
+		} 
+	}
+}
